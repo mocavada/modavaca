ac5b036067578a8835b6cfceb2ba6eebe629da57
mocavada <marc@cyberspace.ph>
2020-07-10 19:04:42 -0400

    push server 4

---
+++ src/main/java/moc/com/spring/modavaca/domains/DigitalProduct.java
@@ -0,0 +1,48 @@
+package moc.com.spring.modavaca.domains;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class DigitalProduct {
+    @Id
+    @GeneratedValue
+    private Integer id;
+
+    @Column
+    private String title;
+
+    @Column(length = 2000)
+    private String description;
+
+    public DigitalProduct(String title, String description) {
+        this.title = title;
+        this.description = description;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
